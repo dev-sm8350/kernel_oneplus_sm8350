611eda6d414706a48488791bcd0b31e398790904
Tyler Nijmeh <tylernij@gmail.com>
2019-08-03 22:58:47 -0700

    net: netfilter: Compile out netfilter log module on !DEBUG_KERNEL
    Change-Id: b430af75de9b6d5de679a8a4e5138ee934e5551c
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- net/netfilter/nf_internals.h
+++ net/netfilter/nf_internals.h
@@ -10,7 +10,11 @@
 void nf_queue_nf_hook_drop(struct net *net);
 
 /* nf_log.c */
+#ifdef CONFIG_DEBUG_KERNEL
 int __init netfilter_log_init(void);
+#else
+static inline int __init netfilter_log_init(void) { return 0; }
+#endif
 
 /* core.c */
 void nf_hook_entries_delete_raw(struct nf_hook_entries __rcu **pp,
