9b1a4cd9ac2c3b098078738112c7de03d3c5a747
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-16 17:03:18 -0800

    techpack: disp: Fix transposed panel_switch and panel_post_switch
    The post switch should be done after the normal switch, not the other
    way around.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I2f6c75c8c6fcde1dc7fa495215a8c974fe8133b0
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -8882,7 +8882,7 @@ int dsi_display_enable(struct dsi_display *display)
 	mode = display->panel->cur_mode;
 
 	if (mode->dsi_mode_flags & DSI_MODE_FLAG_DMS) {
-		rc = dsi_panel_post_switch(display->panel);
+		rc = dsi_panel_switch(display->panel);
 		if (rc) {
 			DSI_ERR("[%s] failed to switch DSI panel mode, rc=%d\n",
 				   display->name, rc);
@@ -8920,7 +8920,7 @@ int dsi_display_enable(struct dsi_display *display)
 			oplus_dsi_display_vsync_switch(display, false);
 		}
 #endif /*OPLUS_BUG_STABILITY*/
-		rc = dsi_panel_switch(display->panel);
+		rc = dsi_panel_post_switch(display->panel);
 		if (rc)
 			DSI_ERR("[%s] failed to switch DSI panel mode, rc=%d\n",
 				   display->name, rc);
