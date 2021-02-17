aadc0df01842e1476b2c43af14708a9689138d86
Viresh Kumar <viresh.kumar@linaro.org>
2021-02-17 11:18:58 +0530

    thermal: cpu_cooling: freq_qos_update_request() returns < 0 on error
    commit a51afb13311cd85b2f638c691b2734622277d8f5 upstream.
    
    freq_qos_update_request() returns 1 if the effective constraint value
    has changed, 0 if the effective constraint value has not changed, or a
    negative error code on failures.
    
    The frequency constraints for CPUs can be set by different parts of the
    kernel. If the maximum frequency constraint set by other parts of the
    kernel are set at a lower value than the one corresponding to cooling
    state 0, then we will never be able to cool down the system as
    freq_qos_update_request() will keep on returning 0 and we will skip
    updating cpufreq_state and thermal pressure.
    
    Fix that by doing the updates even in the case where
    freq_qos_update_request() returns 0, as we have effectively set the
    constraint to a new value even if the consolidated value of the
    actual constraint is unchanged because of external factors.
    
    Cc: v5.7+ <stable@vger.kernel.org> # v5.7+
    Reported-by: Thara Gopinath <thara.gopinath@linaro.org>
    Fixes: f12e4f66ab6a ("thermal/cpu-cooling: Update thermal pressure in case of a maximum frequency capping")
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Change-Id: I1684b8c190c5ac2d29f27a8e26eb31bdc1f7a5e2
    Reviewed-by: Lukasz Luba <lukasz.luba@arm.com>
    Tested-by: Lukasz Luba <lukasz.luba@arm.com>
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Tested-by: Thara Gopinath<thara.gopinath@linaro.org>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/b2b7e84944937390256669df5a48ce5abba0c1ef.1613540713.git.viresh.kumar@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    (cherry picked from commit e045013320a6c74b36c51f36bf17d2fee443f2aa)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -337,7 +337,7 @@ static int cpufreq_set_cur_state(struct thermal_cooling_device *cdev,
 
 	ret = freq_qos_update_request(&cpufreq_cdev->qos_req,
 				      frequency);
-	if (ret > 0) {
+	if (ret >= 0) {
 		cpufreq_cdev->cpufreq_state = state;
 		cpus = cpufreq_cdev->policy->cpus;
 		max_capacity = arch_scale_cpu_capacity(cpumask_first(cpus));
