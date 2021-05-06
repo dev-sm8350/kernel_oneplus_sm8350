0c1a176f836121e20f09d177ba98288e2ac3697c
Sultan Alsawaf <sultan@kerneltoast.com>
2021-05-06 00:12:28 -0700

    sched/fair: Compile out NUMA code entirely when disabled
    Scheduler code is very hot and every little optimization counts. Instead
    of constantly checking sched_numa_balancing when NUMA is disabled,
    compile it out.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I9fab83c9666ad84dc4c9aa91b1b64600a796dce2
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: atndko <z1281552865@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    (cherry picked from commit 1c9dc73cb67ae5a09c0010771c1879e775b0cfbb)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2436,7 +2436,8 @@ void task_numa_fault(int last_cpupid, int mem_node, int pages, int flags)
 	struct numa_group *ng;
 	int priv;
 
-	if (!static_branch_likely(&sched_numa_balancing))
+	if (!IS_ENABLED(CONFIG_NUMA_BALANCING) ||
+	    !static_branch_likely(&sched_numa_balancing))
 		return;
 
 	/* for example, ksmd faulting in a user's mm */
