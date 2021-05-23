900124db7af7dc8c3fa24b5e67d8f47ec857d5f3
root <alexfinhart@gmail.com>
2021-05-23 17:15:19 +0800

    sched: core: Report max frequency to unity tasks
    * More inlining with sunfish
    * Reference: https://android.googlesource.com/device/google/sunfish/+/refs/tags/android-s-beta-1/init.hardware.rc#145
    
    [LeanHijosdesusMadres]:
    * Unity app incorrectly sets cpu affinity to max capacity cores. Setup the library name and mask to always report the same max cpu freq to the tasks.
    * Register more libs to cached max freq
    * Ref: https://github.com/LeanxModulostk/UnityFix
    
    [Nauval Rizky]:
    * Remove UnityMain from sched_lib_name
    sched_lib_name does the job by traversing executable VMA that map a file
    and checking the accessor path with `strnstr()`, so task comm like
    UnityMain will not going to appear anyway.
    
    Signed-off-by: Alex Finhart <alexfinhart@gmail.com>
    Change-Id: I4b1b9b6c3073738259f4511ff002abe0b75b8ebb
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Co-Authored-By: "@LeanHijosdesusMadres" <105339980+LeanxModulostk@users.noreply.github.com>
    Co-Authored-By: Nauval Rizky <enuma.alrizky@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5887,8 +5887,10 @@ out_put_task:
 	return retval;
 }
 
-char sched_lib_name[LIB_PATH_LENGTH];
-unsigned int sched_lib_mask_force;
+char sched_lib_name[LIB_PATH_LENGTH] =
+{ "com.miHoYo.,com.activision.,libunity.so,libil2cpp.so,libmain.so"
+};
+unsigned int sched_lib_mask_force = 255;
 bool is_sched_lib_based_app(pid_t pid)
 {
 	const char *name = NULL;
