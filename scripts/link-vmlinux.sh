d1fddcb1c7e618767c9cbdc743a84d85fccfd0f5
Sami Tolvanen <samitolvanen@google.com>
2021-09-14 22:19:56 +0530

    init: lto: ensure initcall ordering
    With LTO, the compiler doesn't necessarily obey the link order for
    initcalls, and initcall variables need globally unique names to avoid
    collisions at link time.
    
    This change exports __KBUILD_MODNAME and adds the initcall_id() macro,
    which uses it together with __COUNTER__ and __LINE__ to help ensure
    these variables have unique names, and moves each variable to its own
    section when LTO is enabled, so the correct order can be specified using
    a linker script.
    
    The generate_initcall_ordering.pl script uses nm to find initcalls from
    the object files passed to the linker, and generates a linker script
    that specifies the same order for initcalls that we would have without
    LTO. With LTO enabled, the script is called in link-vmlinux.sh through
    jobserver-exec to limit the number of jobs spawned.
    
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Change-Id: I6866edcd9c5bf424d943019001511afb25d89f58
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Link: https://lore.kernel.org/r/20201211184633.3213045-8-samitolvanen@google.com
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -53,6 +53,17 @@ gen_symversions()
 	done
 }
 
+# Generate a linker script to ensure correct ordering of initcalls.
+gen_initcalls()
+{
+	info GEN .tmp_initcalls.lds
+
+	${PYTHON} ${srctree}/scripts/jobserver-exec		\
+	${PERL} ${srctree}/scripts/generate_initcall_order.pl	\
+		${KBUILD_VMLINUX_OBJS} ${KBUILD_VMLINUX_LIBS}	\
+		> .tmp_initcalls.lds
+}
+
 # Link of vmlinux.o used for section mismatch analysis
 # ${1} output file
 modpost_link()
@@ -68,6 +79,9 @@ modpost_link()
 		--end-group"
 
 	if [ -n "${CONFIG_LTO_CLANG}" ] || [ -n "${CONFIG_LTO_GCC}" ]; then
+		gen_initcalls
+		lds="-T .tmp_initcalls.lds"
+
 		if [ -n "${CONFIG_MODVERSIONS}" ]; then
 			gen_symversions
 			lds="${lds} -T .tmp_symversions.lds"
@@ -268,6 +282,7 @@ cleanup()
 {
 	rm -f .btf.*
 	rm -f .tmp_System.map
+	rm -f .tmp_initcalls.lds
 	rm -f .tmp_symversions.lds
 	rm -f .tmp_vmlinux*
 	rm -f System.map
