5105adfd67e745dd028e9c7ae2c87102c92c28f8
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-10-16 15:37:02 +0600

    qcacmn: qdf_trace: Compile out qdf_trace_msg_cmn()
    This was a leftover function after disabling QDF TRACING.
    Use static inline to compile out the remaining tracing code.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: I611d42fd61dc8910709127a842848a01e85356b6
    Signed-off-by: TogoFire <togofire@mailfence.com>>

--- drivers/staging/qca-wifi-host-cmn/qdf/inc/qdf_trace.h
+++ drivers/staging/qca-wifi-host-cmn/qdf/inc/qdf_trace.h
@@ -1323,6 +1323,7 @@ struct category_name_info {
 	unsigned char category_name_str[QDF_MAX_NAME_SIZE];
 };
 
+#ifdef QDF_ENABLE_TRACING
 /**
  * qdf_trace_msg_cmn()- Converged logging API
  * @idx: Index of print control object assigned to the module
@@ -1345,6 +1346,16 @@ void qdf_trace_msg_cmn(unsigned int idx,
 			QDF_TRACE_LEVEL verbose,
 			const char *str_format,
 			va_list val);
+#else
+static inline
+void qdf_trace_msg_cmn(unsigned int idx,
+			QDF_MODULE_ID category,
+			QDF_TRACE_LEVEL verbose,
+			const char *str_format,
+			va_list val)
+{
+}
+#endif
 
 /**
  * struct qdf_print_ctrl: QDF Print Control structure
