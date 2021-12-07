926da5f06a479778004e3e3cb53e94e1ceccf279
Shaleen Agrawal <quic_shalagra@quicinc.com>
2021-12-06 16:36:44 -0800

    sched/walt: Increase the number of history samples
    The number history samples should be raised to a higher value so that an
    active history of 100ms is considered in accounting task demand. Raise
    the history size from 5 to 8 given the default windows size is 8ms. This
    will capture 64ms of history for 8ms windows, and 128ms of history for
    16ms windows, granting a compromise to prevent further code bloat.
    
    Signed-off-by: Shaleen Agrawal <quic_shalagra@quicinc.com>
    Change-Id: Id75128d87bb33c85d1073e7721fbc4e7a73de023
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    (cherry picked from commit abf868670c73878409371257909cd313ab879bf4)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/sched/walt/walt.c
+++ kernel/sched/walt/walt.c
@@ -143,7 +143,7 @@ __read_mostly unsigned int sysctl_sched_asym_cap_sibling_freq_match_pct = 100;
 __read_mostly unsigned int sysctl_sched_asym_cap_sibling_freq_match_en;
 static cpumask_t asym_freq_match_cpus = CPU_MASK_NONE;
 
-static __read_mostly unsigned int sched_ravg_hist_size = 5;
+static __read_mostly unsigned int sched_ravg_hist_size = RAVG_HIST_SIZE_MAX;
 
 static __read_mostly unsigned int sched_io_is_busy = 1;
 
