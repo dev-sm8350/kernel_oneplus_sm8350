7138937f42fe3e97d74151118a1222d280858625
Mohammed Ahmed <quic_moah@quicinc.com>
2022-08-24 13:07:07 -0700

    qcacmn: add psoc null pointer check to wlan_objmgr_get_peer_by_mac
    Current code directly passes the result of wlan_vdev_get_psoc(vdev) to
    wlan_objmgr_get_peer_by_mac, without null pointer checking, which can
    result in null pointer dereferencing.
    To fix this issue, add checking of the psoc pointer before passing,
    and add verification inside wlan_objmgr_get_peer_by_mac.
    
    CRs-Fixed: 3277651
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: I706f966ecefb9ed4d02d9f8209009cea261c69d2
    (cherry picked from commit 05bc8f703edcc103a03b8a95857a4cde9bbc339b)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/staging/qca-wifi-host-cmn/umac/cmn_services/obj_mgr/src/wlan_objmgr_psoc_obj.c
+++ drivers/staging/qca-wifi-host-cmn/umac/cmn_services/obj_mgr/src/wlan_objmgr_psoc_obj.c
@@ -2102,6 +2102,8 @@ struct wlan_objmgr_peer *wlan_objmgr_get_peer_by_mac_debug(
 
 	if (!macaddr)
 		return NULL;
+	if (!psoc)
+		return NULL;
 
 	/* psoc lock should be taken before peer list lock */
 	wlan_psoc_obj_lock(psoc);
@@ -2137,6 +2139,8 @@ struct wlan_objmgr_peer *wlan_objmgr_get_peer_by_mac(
 	struct wlan_peer_list *peer_list;
 
 	if (!macaddr)
+		return NULL;
+	if (!psoc)
 		return NULL;
 
 	/* psoc lock should be taken before peer list lock */
