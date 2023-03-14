8dc66482c6a6d0fb8e2487379cd235d44d67355c
Vaisakh Murali <mvaisakh@statixos.com>
2023-03-14 14:20:56 +0600

    drivers/misc: tri_state_key: checkout from sm8250
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: I930a4b49fd7e88b88a85d7c6a233be0fe7bea41f
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/misc/tri_state_key/oplus_tri_key.c
+++ drivers/misc/tri_state_key/oplus_tri_key.c
@@ -71,7 +71,6 @@ unsigned int tristate_extcon_tab[] = {
 		MODE_MUTE,
 		MODE_DO_NOT_DISTURB,
 		MODE_NORMAL,
-		EXTCON_NONE,
 	};
 
 static struct hrtimer tri_key_timer;
@@ -525,7 +524,7 @@ static int judge_interference(struct extcon_dev_data *chip)
 				}
 			}
 		if (delta > calib_DnValueMin - down_tolerance &&
-			delta < calib_DnValueMin + up_mid_tol) {
+			delta < calib_DnValueMin + mid_down_tol) {
 			TRI_KEY_LOG("calib_Min:%d,calib_Sum:%d\n",
 				calib_DnValueMin, calib_DnValueSum);
 
@@ -1039,18 +1038,17 @@ void initialCalibValue(short calib_dnHall_UpV, short calib_dnHall_MdV,
 	calib_upHall_UD_distance = Minus(calib_upHall_UpV, calib_upHall_DnV);
 	calib_dnHall_UD_distance = Minus(calib_dnHall_UpV, calib_dnHall_DnV);
 	up_mid_tol = (short)(abs(calib_UpValueMin - calib_MdValueMin)
-			* 4 / 10);
+		* 4 / 10);
 	up_tolerance = (short)(abs(calib_UpValueMin - calib_MdValueMin)
-			* 11 / 10);
+		* 11 / 10);
 	mid_down_tol = (short)(abs(calib_MdValueMin - calib_DnValueMin)
-			* 4 / 10);
+		* 4 / 10);
 	down_tolerance = (short)(abs(calib_MdValueMin -
-				calib_DnValueMin) * 11 / 10);
+		calib_DnValueMin) * 11 / 10);
 	up_mid_distance = (short)(abs(calib_UpValueMin -
-				calib_MdValueMin) * 2 / 10);
+		calib_MdValueMin) * 2 / 10);
 	mid_down_distance = (short)(abs(calib_MdValueMin -
-				calib_DnValueMin) * 2 / 10);
-
+		calib_DnValueMin) * 2 / 10);
 	TRI_KEY_LOG("Upmin:%d, Mdmin:%d, Dnmin:%d\n",
 		calib_UpValueMin, calib_MdValueMin, calib_DnValueMin);
 	TRI_KEY_LOG("up_mid_tol:%d, mid_down_tol:%d\n",
@@ -1211,31 +1209,6 @@ static const struct file_operations proc_hall_debug_info_ops = {
 	.owner = THIS_MODULE,
 };
 
-static ssize_t proc_hall_enable_irq_write(struct file *file, const char __user *buffer,
-			size_t count, loff_t *ppos)
-{
-	int tmp = 0;
-	char buf[4] = {0};
-
-	if (count > 2)
-		return count;
-	copy_from_user(buf, buffer, count);
-	if (!kstrtoint(buf, 0, &tmp)) {
-		oplus_hall_enable_irq(0, tmp);
-		oplus_hall_enable_irq(1, tmp);
-	} else
-		TRI_KEY_DEBUG("invalid content: '%s', length = %zd\n",
-		buf, count);
-
-	return count;
-}
-
-static const struct file_operations proc_hall_enable_irq_ops = {
-	.write = proc_hall_enable_irq_write,
-	.open  = simple_open,
-	.owner = THIS_MODULE,
-};
-
 static int init_trikey_proc(struct extcon_dev_data *hall_dev)
 {
 	int ret = 0;
@@ -1275,14 +1248,8 @@ static int init_trikey_proc(struct extcon_dev_data *hall_dev)
 		TRI_KEY_ERR("%s: Couldn't create proc entry, %d\n", __func__, __LINE__);
 	}
 
-	prEntry_tmp = proc_create("hall_enable_irq", 0666, prEntry_trikey,
-			&proc_hall_enable_irq_ops);
-	if (prEntry_tmp == NULL) {
-		ret = -ENOMEM;
-		TRI_KEY_ERR("%s: Couldn't create proc entry, %d\n", __func__, __LINE__);
-	}
-
 	return ret;
+
 }
 static void register_tri_key_dev_work(struct work_struct *work)
 {
@@ -1371,7 +1338,6 @@ static void register_tri_key_dev_work(struct work_struct *work)
 	}
 	err = oplus_hall_set_detection_mode(DHALL_1,
 			DETECTION_MODE_INTERRUPT);
-
 	TRI_KEY_LOG("tri_key:set 1 detection mode\n");
 	if (err < 0) {
 		TRI_KEY_ERR("%s set HALL1 detection mode failed %d\n",
