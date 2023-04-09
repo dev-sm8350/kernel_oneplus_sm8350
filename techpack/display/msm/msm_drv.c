39d720b998ff6f31c27e8d820fd0c2ce5e683879
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-04-09 21:16:38 +0530

    techpack/display: msm: Set idle state correctly
    Set the idle state based on sde encoder's rc state.
    
    Test:
    watch -n 0.1 cat /sys/class/drm/card0/device/idle_state
    - shows "active" when display is being updated
    - "idle" otherwise
    
    (cherry picked from commit b746ea4e6fda9cb20f743b32899ec3561208e231)
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I80ddc108d6d0068fa79562214df6e4007eb0e913

--- techpack/display/msm/msm_drv.c
+++ techpack/display/msm/msm_drv.c
@@ -51,6 +51,7 @@
 #include "msm_mmu.h"
 #include "sde_wb.h"
 #include "sde_dbg.h"
+#include "sde/sde_encoder.h"
 
 #ifdef OPLUS_BUG_STABILITY
 #include "oplus_adfr.h"
@@ -80,7 +81,7 @@
 #define LASTCLOSE_TIMEOUT_MS	500
 
 #define IDLE_ENCODER_MASK_DEFAULT	2
-#define IDLE_TIMEOUT_MS_DEFAULT		100
+#define IDLE_TIMEOUT_MS_DEFAULT		100 - IDLE_POWERCOLLAPSE_DURATION
 
 #define msm_wait_event_timeout(waitq, cond, timeout_ms, ret)		\
 	do {								\
