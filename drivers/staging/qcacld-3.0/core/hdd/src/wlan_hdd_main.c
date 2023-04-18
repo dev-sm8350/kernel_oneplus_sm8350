a8ee3c34190270069bd0e1dedc6d0159fae67e87
Vinod Kumar Pirla <quic_vpirla@quicinc.com>
2023-04-18 08:46:15 -0700

    qcacld-3.0: Check NULL value of hdd_adapter_get_mac_handle()
    The return of hdd_adapter_get_mac_handle() API can be NULL.
    Check the NULL pointer before accessing.
    
    CRs-Fixed: 3463354
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: Id5ae49a1769d8b5cf91b22130a0d3e526fa2a3ef
    (cherry picked from commit 5022612ccf2140c9f50ce288f7e15b17158f936e)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -18980,6 +18980,11 @@ int hdd_get_rssi_snr_by_bssid(struct hdd_adapter *adapter, const uint8_t *bssid,
 
 	roam_profile = hdd_roam_profile(adapter);
 	mac_handle = hdd_adapter_get_mac_handle(adapter);
+	if (!mac_handle) {
+		hdd_err("mac context NULL");
+		return -EINVAL;
+	}
+
 	status = sme_get_rssi_snr_by_bssid(mac_handle,
 					   roam_profile, bssid, rssi, snr,
 					   adapter->vdev_id);
