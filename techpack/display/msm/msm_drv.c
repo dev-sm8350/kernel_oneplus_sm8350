c042e1e627ad51957c0dea48c32f72a9e02834d9
Alexander Winkowski <dereference23@outlook.com>
2023-06-11 16:16:25 +0000

    disp: msm: Micro-optimize IRQ handlers
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Change-Id: I3f4a38108287a808d0e6c5fdf1b8e420e2a5330a
    (cherry picked from commit dfb77da133933d354c5f8b7856e82e633bf8139d)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- techpack/display/msm/msm_drv.c
+++ techpack/display/msm/msm_drv.c
@@ -1328,7 +1328,6 @@ static irqreturn_t msm_irq(int irq, void *arg)
 	struct drm_device *dev = arg;
 	struct msm_drm_private *priv = dev->dev_private;
 	struct msm_kms *kms = priv->kms;
-	BUG_ON(!kms);
 	return kms->funcs->irq(kms);
 }
 
@@ -1336,7 +1335,6 @@ static void msm_irq_preinstall(struct drm_device *dev)
 {
 	struct msm_drm_private *priv = dev->dev_private;
 	struct msm_kms *kms = priv->kms;
-	BUG_ON(!kms);
 	kms->funcs->irq_preinstall(kms);
 }
 
@@ -1344,7 +1342,6 @@ static int msm_irq_postinstall(struct drm_device *dev)
 {
 	struct msm_drm_private *priv = dev->dev_private;
 	struct msm_kms *kms = priv->kms;
-	BUG_ON(!kms);
 
 	if (kms->funcs->irq_postinstall)
 		return kms->funcs->irq_postinstall(kms);
@@ -1356,7 +1353,6 @@ static void msm_irq_uninstall(struct drm_device *dev)
 {
 	struct msm_drm_private *priv = dev->dev_private;
 	struct msm_kms *kms = priv->kms;
-	BUG_ON(!kms);
 	kms->funcs->irq_uninstall(kms);
 }
 
