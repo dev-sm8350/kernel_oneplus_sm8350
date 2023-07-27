80b960ea9b66be0387d43d3cbe23171b1e346632
TogoFire <togofire@mailfence.com>
2023-07-27 18:32:56 -0300

    drivers: staging: kernelsu: Silence spam logs
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I4e5b848a5ab6f1a1abd8c1c46d4da8a2a96b21bd

--- drivers/staging/ksu/core_hook.c
+++ drivers/staging/ksu/core_hook.c
@@ -317,7 +317,7 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 				ksu_version = vmin_ksu;
 			u32 version = (u32) ksu_version;
 			if (copy_to_user(arg3, &version, sizeof(version))) {
-				pr_err("prctl reply error, cmd: %lu\n", arg2);
+				pr_debug("prctl reply error, cmd: %lu\n", arg2);
 			}
 		}
 		return 0;
@@ -396,7 +396,7 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 						  sizeof(u32) * array_length)) {
 					if (copy_to_user(result, &reply_ok,
 							 sizeof(reply_ok))) {
-						pr_err("prctl reply error, cmd: %lu\n",
+						pr_debug("prctl reply error, cmd: %lu\n",
 						       arg2);
 					}
 				} else {
@@ -421,7 +421,7 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 			if (!copy_to_user(arg4, &allow, sizeof(allow))) {
 				if (copy_to_user(result, &reply_ok,
 						 sizeof(reply_ok))) {
-					pr_err("prctl reply error, cmd: %lu\n",
+					pr_debug("prctl reply error, cmd: %lu\n",
 					       arg2);
 				}
 			} else {
@@ -452,7 +452,7 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 				return 0;
 			}
 			if (copy_to_user(result, &reply_ok, sizeof(reply_ok))) {
-				pr_err("prctl reply error, cmd: %lu\n", arg2);
+				pr_debug("prctl reply error, cmd: %lu\n", arg2);
 			}
 		}
 		return 0;
@@ -468,7 +468,7 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 		// todo: validate the params
 		if (ksu_set_app_profile(&profile, true)) {
 			if (copy_to_user(result, &reply_ok, sizeof(reply_ok))) {
-				pr_err("prctl reply error, cmd: %lu\n", arg2);
+				pr_debug("prctl reply error, cmd: %lu\n", arg2);
 			}
 		}
 		return 0;
@@ -494,7 +494,7 @@ static bool should_umount(struct path *path)
 	}
 
 	if (current->nsproxy->mnt_ns == init_nsproxy.mnt_ns) {
-		pr_info("ignore global mnt namespace process: %d\n",
+		pr_debug("ignore global mnt namespace process: %d\n",
 			current_uid().val);
 		return false;
 	}
@@ -582,7 +582,7 @@ int ksu_handle_setuid(struct cred *new, const struct cred *old)
 		return 0;
 	}
 	// umount the target mnt
-	pr_info("handle umount for uid: %d, pid: %d\n", new_uid.val,
+	pr_debug("handle umount for uid: %d, pid: %d\n", new_uid.val,
 		current->pid);
 
 	// fixme: use `collect_mounts` and `iterate_mount` to iterate all mountpoint and
