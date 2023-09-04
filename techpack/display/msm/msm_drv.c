392686a61ddb82f49daee83abc35294990153742
Vishalcj17 <vishalcj@aospa.co>
2023-09-05 01:13:59 +0900

    techpack: display: Nuke pixelworks completely
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ib53f9b2c16302cfad3659ff0eeb26933f0cc6e6e

--- techpack/display/msm/msm_drv.c
+++ techpack/display/msm/msm_drv.c
@@ -50,9 +50,6 @@
 #include "msm_mmu.h"
 #include "sde_wb.h"
 #include "sde_dbg.h"
-#if defined(OPLUS_FEATURE_PXLW_IRIS5) || defined(OPLUS_FEATURE_PXLW_SOFT_IRIS)
-#include "dsi/iris/dsi_iris5_api.h"
-#endif
 
 #ifdef OPLUS_BUG_STABILITY
 #include "oplus_adfr.h"
@@ -1117,12 +1114,6 @@ static void msm_lastclose(struct drm_device *dev)
 		if (!rc)
 			DRM_INFO("wait for crtc mask 0x%x failed, commit anyway...\n",
 				priv->pending_crtcs);
-
-#ifndef OPLUS_FEATURE_PXLW_IRIS5
-		rc = kms->funcs->trigger_null_flush(kms);
-		if (rc)
-			return;
-#endif
 	}
 
 	/*
@@ -1815,10 +1806,6 @@ static const struct drm_ioctl_desc msm_ioctls[] = {
 	DRM_IOCTL_DEF_DRV(MSM_RMFB2, msm_ioctl_rmfb2, DRM_UNLOCKED),
 	DRM_IOCTL_DEF_DRV(MSM_POWER_CTRL, msm_ioctl_power_ctrl,
 			DRM_RENDER_ALLOW),
-#if defined(OPLUS_FEATURE_PXLW_IRIS5) || defined(OPLUS_FEATURE_PXLW_SOFT_IRIS)
-	DRM_IOCTL_DEF_DRV(MSM_IRIS_OPERATE_CONF, msm_ioctl_iris_operate_conf, DRM_UNLOCKED|DRM_RENDER_ALLOW),
-	DRM_IOCTL_DEF_DRV(MSM_IRIS_OPERATE_TOOL, msm_ioctl_iris_operate_tool, DRM_UNLOCKED|DRM_RENDER_ALLOW),
-#endif
 	DRM_IOCTL_DEF_DRV(MSM_DISPLAY_HINT, msm_ioctl_display_hint_ops,
 			DRM_UNLOCKED),
 };
