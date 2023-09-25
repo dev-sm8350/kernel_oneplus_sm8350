7ce93038dd3f728c8097aeddc4b19ce1ae796bf5
Leo <leohuangyi@foxmail.com>
2023-09-26 03:02:04 +0800

    drivers: Providing kernel support for okcar
    1. Simulated USB descriptors of iPhone
    2. Responding to car's USB requests following the CarPlay protocol specifications
    3. Implementing basic IAP2 communication to support some car
    4. Support usb mode toggle
    
    ref: https://github.com/okcar-os
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ia7089823e604fba817f256c3fbb0dc1c401c5c2f

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -1639,7 +1639,10 @@ static int android_setup(struct usb_gadget *gadget,
 	spin_lock_irqsave(&cdev->lock, flags);
 	if (c->bRequest == USB_REQ_SET_CONFIGURATION &&
 						cdev->config) {
-		schedule_work(&gi->work);
+		// avoid 21.09(HID_REQ_SET_REPORT) trigger this
+		if ((c->bRequestType & USB_TYPE_MASK) == USB_TYPE_STANDARD) {
+			schedule_work(&gi->work);
+		}
 	}
 	spin_unlock_irqrestore(&cdev->lock, flags);
 
