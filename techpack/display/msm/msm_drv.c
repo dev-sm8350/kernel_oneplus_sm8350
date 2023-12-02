f8a3e340c7061fd73b18a55ef0225982b7c7e2f5
LuK1337 <priv.luk@gmail.com>
2023-12-02 23:21:20 +0100

    disp: msm: Skip null flush on Pixelworks Iris 5 devices
    It seems that it breaks touchscreen on boot.
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I68775c5ee0328e9e12f38f2300e37b043dfa6307

--- techpack/display/msm/msm_drv.c
+++ techpack/display/msm/msm_drv.c
@@ -1278,9 +1278,11 @@ static void msm_lastclose(struct drm_device *dev)
 			DRM_INFO("wait for crtc mask 0x%x failed, commit anyway...\n",
 				priv->pending_crtcs);
 
+#ifndef OPLUS_FEATURE_PXLW_IRIS5
 		rc = kms->funcs->trigger_null_flush(kms);
 		if (rc)
 			return;
+#endif
 	}
 
 	/*
