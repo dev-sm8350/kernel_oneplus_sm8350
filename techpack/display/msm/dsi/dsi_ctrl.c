8e55cb8963437f671bdcd313e300e5c4bd90d3ca
TogoFire <togofire@mailfence.com>
2024-01-30 15:19:37 -0300

    Revert "drm/msm: dsi-ctrl: remove extra buffer copy"
    This reverts commit 0211e47c2abd54d7440369a04d91a61758f91e8e.
    
    Fix: When locking the lock screen, the screen goes black.
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ica5e4ea87e942c85a002b7ada1f4dc9ee33e04f4

--- techpack/display/msm/dsi/dsi_ctrl.c
+++ techpack/display/msm/dsi/dsi_ctrl.c
@@ -1145,21 +1145,31 @@ error:
 	return rc;
 }
 
-static int dsi_ctrl_copy_and_pad_cmd(const struct mipi_dsi_packet *packet,
-				     u8 *buf, size_t len)
+static int dsi_ctrl_copy_and_pad_cmd(struct dsi_ctrl *dsi_ctrl,
+				     const struct mipi_dsi_packet *packet,
+				     u8 **buffer,
+				     u32 *size)
 {
 	int rc = 0;
+	u8 *buf = NULL;
+	u32 len, i;
 	u8 cmd_type = 0;
 
-	if (unlikely(len < packet->size))
-		return -EINVAL;
+	len = packet->size;
+	len += 0x3; len &= ~0x03; /* Align to 32 bits */
 
-	memcpy(buf, packet->header, sizeof(packet->header));
-	if (packet->payload_length)
-		memcpy(buf + sizeof(packet->header), packet->payload,
-		       packet->payload_length);
-	if (packet->size < len)
-		memset(buf + packet->size, 0xFF, len - packet->size);
+	buf = devm_kzalloc(&dsi_ctrl->pdev->dev, len * sizeof(u8), GFP_KERNEL);
+	if (!buf)
+		return -ENOMEM;
+
+	for (i = 0; i < len; i++) {
+		if (i >= packet->size)
+			buf[i] = 0xFF;
+		else if (i < sizeof(packet->header))
+			buf[i] = packet->header[i];
+		else
+			buf[i] = packet->payload[i - sizeof(packet->header)];
+	}
 
 	if (packet->payload_length > 0)
 		buf[3] |= BIT(6);
@@ -1176,6 +1186,9 @@ static int dsi_ctrl_copy_and_pad_cmd(const struct mipi_dsi_packet *packet,
 			(cmd_type == MIPI_DSI_GENERIC_READ_REQUEST_1_PARAM) ||
 			(cmd_type == MIPI_DSI_GENERIC_READ_REQUEST_2_PARAM))
 		buf[3] |= BIT(5);
+
+	*buffer = buf;
+	*size = len;
 
 	return rc;
 }
@@ -1608,8 +1621,9 @@ static int dsi_message_tx(struct dsi_ctrl *dsi_ctrl,
 	struct mipi_dsi_packet packet;
 	struct dsi_ctrl_cmd_dma_fifo_info cmd;
 	struct dsi_ctrl_cmd_dma_info cmd_mem;
-	u32 length;
+	u32 length = 0;
 	u8 *buffer = NULL;
+	u32 cnt = 0;
 	u8 *cmdbuf;
 
 	/*#ifdef OPLUS_BUG_STABILITY*/
@@ -1673,7 +1687,14 @@ static int dsi_message_tx(struct dsi_ctrl *dsi_ctrl,
 		goto error;
 	}
 
-	length = ALIGN(packet.size, 4);
+	rc = dsi_ctrl_copy_and_pad_cmd(dsi_ctrl,
+			&packet,
+			&buffer,
+			&length);
+	if (rc) {
+		DSI_CTRL_ERR(dsi_ctrl, "failed to copy message, rc=%d\n", rc);
+		goto error;
+	}
 
 	/*
 	 * In case of broadcast CMD length cannot be greater than 512 bytes
@@ -1692,7 +1713,7 @@ static int dsi_message_tx(struct dsi_ctrl *dsi_ctrl,
 
 	if ((msg->flags & MIPI_DSI_MSG_LASTCOMMAND) ||
 			(*flags & DSI_CTRL_CMD_LAST_COMMAND))
-		packet.header[3] |= BIT(7);//set the last cmd bit in header.
+		buffer[3] |= BIT(7);//set the last cmd bit in header.
 
 	if (*flags & DSI_CTRL_CMD_FETCH_MEMORY) {
 		/* Embedded mode config is selected */
@@ -1706,6 +1727,10 @@ static int dsi_message_tx(struct dsi_ctrl *dsi_ctrl,
 
 		cmdbuf = (u8 *)(dsi_ctrl->vaddr);
 
+		msm_gem_sync(dsi_ctrl->tx_cmd_buf);
+		for (cnt = 0; cnt < length; cnt++)
+			cmdbuf[dsi_ctrl->cmd_len + cnt] = buffer[cnt];
+
 		dsi_ctrl->cmd_len += length;
 
 		if (!(msg->flags & MIPI_DSI_MSG_LASTCOMMAND) &&
@@ -1717,20 +1742,6 @@ static int dsi_message_tx(struct dsi_ctrl *dsi_ctrl,
 		}
 
 	} else if (*flags & DSI_CTRL_CMD_FIFO_STORE) {
-		buffer = devm_kzalloc(&dsi_ctrl->pdev->dev, length,
-					   GFP_KERNEL);
-		if (!buffer) {
-			rc = -ENOMEM;
-			goto error;
-		}
-
-		rc = dsi_ctrl_copy_and_pad_cmd(&packet, buffer, length);
-		if (rc) {
-			DSI_CTRL_ERR(dsi_ctrl, "failed to copy message, rc=%d\n",
-					rc);
-			goto error;
-		}
-
 		cmd.command =  (u32 *)buffer;
 		cmd.size = length;
 		cmd.en_broadcast = (*flags & DSI_CTRL_CMD_BROADCAST) ?
