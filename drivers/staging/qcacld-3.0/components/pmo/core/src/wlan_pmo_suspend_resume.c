99fe323643eb829016e10c78d6a8c7eb41be7e9a
Shashank Reddy Vulupala <quic_svulupal@quicinc.com>
2024-03-18 17:45:05 +0530

    qcacld-3.0: Add NULL check for hif_ctx
    This change is to add NULL check for hif_ctx.
    
    CRs-Fixed: 3776529
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: Iddece317c82eba59b6e0fd4ad77056ccd123a614
    (cherry picked from commit 83578443c24ce761ea771e9903e9ef9776281966)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/staging/qcacld-3.0/components/pmo/core/src/wlan_pmo_suspend_resume.c
+++ drivers/staging/qcacld-3.0/components/pmo/core/src/wlan_pmo_suspend_resume.c
@@ -1349,6 +1349,11 @@ QDF_STATUS pmo_core_psoc_send_host_wakeup_ind_to_fw(
 	pmo_enter();
 
 	hif_ctx = pmo_core_psoc_get_hif_handle(psoc);
+	if (!hif_ctx) {
+		pmo_err("hif_ctx is NULL");
+		status = QDF_STATUS_E_INVAL;
+		goto out;
+	}
 
 	hif_set_ep_intermediate_vote_access(hif_ctx);
 
