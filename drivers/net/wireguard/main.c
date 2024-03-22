123d3071de4f0cb27dc60b7b7556576edf8004bc
Iurii Egorov <ye@amnezia.org>
2024-03-22 18:37:03 +0300

    wireguard: Add AmneziaWG protocol improvements
    * Add Amnezia VPN protocol
    * Ref: https://github.com/amnezia-vpn/amneziawg-linux-kernel-module
    https://github.com/amnezia-vpn/amneziawg-linux-kernel-module/commit/baf34a93ce071057babc88b3333cd7b8ffd357ba
    https://amnezia.org/en
    * Fix incorrect post config empty asc handling
    * Improve parameter handling and add handshake debugging
    * Small cosmetic changes
    
    [TogoFire]: Adapted to k5.4 and keep the name wireguard.
    
    Signed-off-by: pokamest <pokamest@gmail.com>
    Change-Id: I86c7da20534f3a4fa093df4ad6d2adffe11ff455
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Co-Authored-By: pokamest <pokamest@gmail.com>

--- drivers/net/wireguard/main.c
+++ drivers/net/wireguard/main.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2015-2019 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ * Copyright (C) 2024 AmneziaVPN <admin@amnezia.org>. All Rights Reserved.
  */
 
 #include "version.h"
@@ -45,8 +46,9 @@ static int __init wg_mod_init(void)
 	if (ret < 0)
 		goto err_netlink;
 
-	pr_info("WireGuard " WIREGUARD_VERSION " loaded. See www.wireguard.com for information.\n");
+	pr_info("WireGuard " WIREGUARD_VERSION " loaded. See www.wireguard.com or amnezia.org for information.\n");
 	pr_info("Copyright (C) 2015-2019 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.\n");
+	pr_info("Copyright (C) 2024 AmneziaVPN <admin@amnezia.org>. All Rights Reserved.\n");
 
 	return 0;
 
@@ -72,7 +74,7 @@ module_init(wg_mod_init);
 module_exit(wg_mod_exit);
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("WireGuard secure network tunnel");
-MODULE_AUTHOR("Jason A. Donenfeld <Jason@zx2c4.com>");
+MODULE_AUTHOR("Jason A. Donenfeld <Jason@zx2c4.com>, AmneziaVPN <admin@amnezia.org>");
 MODULE_VERSION(WIREGUARD_VERSION);
 MODULE_ALIAS_RTNL_LINK(KBUILD_MODNAME);
 MODULE_ALIAS_GENL_FAMILY(WG_GENL_NAME);
