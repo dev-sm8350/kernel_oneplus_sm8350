fe150e17f33836fdbad64a0c6afc48b4bdca4033
TogoFire <togofire@mailfence.com>
2024-05-16 01:21:27 -0300

    fs: exfat: Update exFAT driver
    git-repository: https://github.com/namjaejeon/linux-exfat-oot.git
    Date: Mar 1, 2024
    Last commit: exfat: fix appending discontinuous clusters to empty file
    
    Note:
    Your device didn't come with exFat, you can't add extra official support, you can't include something that doesn't have it in stock, that's for Lineage (their device requirements) only.
    
    SDFat is old, so let's move on to something more up-to-date and with better performance.
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I43c5b67c693285a6fb5db7997cfbc88d2f26ba3e

--- fs/exfat/balloc.c
+++ fs/exfat/balloc.c
@@ -8,11 +8,7 @@
 #include <linux/slab.h>
 #include <linux/bitmap.h>
 #include <linux/buffer_head.h>
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
 #include <linux/sched/signal.h>
-#else
-#include <linux/sched.h>
-#endif
 #include <linux/vmalloc.h>
 
 #include "exfat_raw.h"
@@ -29,10 +25,6 @@
 #else
 #error "BITS_PER_LONG not 32 or 64"
 #endif
-
-#ifndef ALIGN_DOWN
-#define ALIGN_DOWN(x, a) __ALIGN_KERNEL((x) - ((a) - 1), (a))
-#endif 
 
 /*
  *  Allocation Bitmap Management Functions
@@ -61,12 +53,8 @@ static int exfat_allocate_bitmap(struct super_block *sb,
 	}
 	sbi->map_sectors = ((need_map_size - 1) >>
 			(sb->s_blocksize_bits)) + 1;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
 	sbi->vol_amap = kvmalloc_array(sbi->map_sectors,
 				sizeof(struct buffer_head *), GFP_KERNEL);
-#else
-	sbi->vol_amap = vmalloc(sbi->map_sectors * sizeof(struct buffer_head *));
-#endif
 	if (!sbi->vol_amap)
 		return -ENOMEM;
 
