6c4e68861e1686937a82478545d63e23836a57c6
EmanuelCN <emanuelghub@gmail.com>
2024-06-10 18:15:35 +0300

    sched: Implement tapered dvfs_headroom
    The linear one is very aggressive at the top frequencies. 25% headroom
    means that we saturate the cluster very quickly. Given how expensive the
    new clusters are at the top frequencies, it'd be smarter to taper the
    headroom as we approach the max capacity of the cluster.
    
    Implement that by simply taking the headroom of the different between
    curret util and capacity of the CPU. The lower frequencies will get
    a big headroom, but as we approach max capacity, we'll get almost no
    headroom.
    
    Only CFS + RT utilization use the tapered version. irq is usually small
    and this can lead to potentially undesired boost almost when there are
    no irqs running. DL shouldn't need any headroom in general. So keeping
    it using the default non tapered version as well.
    
    Ported from: https://github.com/LineageOS/android_kernel_google_gs201/commit/752c5f9
    
    [EmanuelCN: removed per cpu capacity margin and vendor hooks]
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>
    Change-Id: Ieb7006ec75f5b09c0735f208f35a97d6d2394c55
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>
    (cherry picked from commit eef91ce43dffaa902e6f21b1421490573d44503f)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -276,8 +276,10 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 * frequency will be gracefully reduced with the utilization decay.
 	 */
 	util = util_cfs + cpu_util_rt(rq);
-	if (type == FREQUENCY_UTIL)
+	if (type == FREQUENCY_UTIL) {
+		util = apply_dvfs_headroom(util, cpu, true);
 		util = uclamp_rq_util_with(rq, util, p);
+	}
 
 	dl_util = cpu_util_dl(rq);
 
@@ -310,7 +312,7 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 *                 max
 	 */
 	util = scale_irq_capacity(util, irq, max);
-	util += irq;
+	util += type == FREQUENCY_UTIL ? apply_dvfs_headroom(irq, cpu, false) : irq;
 
 	/*
 	 * Bandwidth required by DEADLINE must always be granted while, for
@@ -323,7 +325,7 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 * an interface. So, we only do the latter for now.
 	 */
 	if (type == FREQUENCY_UTIL)
-		util += cpu_bw_dl(rq);
+		util += apply_dvfs_headroom(cpu_bw_dl(rq), cpu, false);
 
 	return min(max, util);
 }
