f606f43662d3b1edf38a3acb514bfaa74a7edae0
Qais Yousef <qyousef@layalina.io>
2024-06-11 23:46:56 +0300

    sched: cpufreq: Apply DVFS headroom to CFS only
    RT and Deadline have exact performance requirement when running. RT runs
    at max or a specific OPP defined by uclamp_min. Deadline's OPP is
    defined by its bandwidth. Both of which are known ahead of time and
    don't require a headroom to grow into.
    
    IRQs on the other hand have no specific performance requirement and
    cruises along at whatever the current OPP happens to be when they occur.
    
    Now they all have PELT pressure signals that does impact frequency
    selection and task placement. The question is do they need DVFS
    headroom?
    
    I think the answer is no because when CFS is not running at all, these
    pressure signal has no real impact on performance for RT, DL or IRQ.
    
    If CFS util is not zero, we already add their pressure as an
    *additional* headroom to account for the lost/stolen time. So I argue
    that the pressure are headroom themselves and shouldn't need an
    additional DVFS headroom applied on top.
    
    In summary final outcome should be:
    
    	CFS + DVFS headroom + (RT, DT, IRQ) pressure headroom
    
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: Ic86cbac05c8ca176265cdeb5ea2864d3f4e9d0e7
    [EmanuelCN: Enable tapered headroom out of the box]
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>
    (cherry picked from commit 150ec63ebf393dd5b323ba2d9fc2bab944a7a23c)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11919,29 +11919,25 @@ static void propagate_entity_cfs_rq(struct sched_entity *se) { }
 
 unsigned int sched_dvfs_headroom = 1280;
 
-unsigned long apply_dvfs_headroom(unsigned long util, int cpu, bool tapered)
+unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 {
-	if (tapered) {
-		unsigned long capacity = capacity_orig_of(cpu);
-		unsigned long headroom;
+	unsigned long capacity = capacity_orig_of(cpu);
+	unsigned long headroom;
 
-		if (util >= capacity)
-			return util;
+	if (util >= capacity)
+		return util;
 
-		/*
-		 * Taper the boosting at e top end as these are expensive and
-		 * we don't need that much of a big headroom as we approach max
-		 * capacity
-		 *
-		 */
-		headroom = (capacity - util);
-		/* formula: headroom * (1.X - 1) == headroom * 0.X */
-		headroom = headroom *
-			(sched_dvfs_headroom - SCHED_CAPACITY_SCALE) >> SCHED_CAPACITY_SHIFT;
-		return util + headroom;
-	}
-
-	return util * sched_dvfs_headroom >> SCHED_CAPACITY_SHIFT;
+	/*
+	 * Taper the boosting at the top end as these are expensive and
+	 * we don't need that much of a big headroom as we approach max
+	 * capacity
+	 *
+	 */
+	headroom = (capacity - util);
+	/* formula: headroom * (1.X - 1) == headroom * 0.X */
+	headroom = headroom *
+		(sched_dvfs_headroom - SCHED_CAPACITY_SCALE) >> SCHED_CAPACITY_SHIFT;
+	return util + headroom;
 }
 
 static void detach_entity_cfs_rq(struct sched_entity *se)
