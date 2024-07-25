27d1804cab81bf6f5b81e981ae806798dbe8580f
TogoFire <togofire@mailfence.com>
2024-07-25 15:22:36 -0300

    Revert "kernel: Remove sched_lib hacks"
    This reverts commit 3ef41f659a75225291156c16474ed32f6b59ca88.
    
    Reason: Causes a drop in performance.
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: If045ae3c49d551c178924eaecd9634d1c4ef4e6d

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -813,6 +813,24 @@ static struct ctl_table kern_table[] = {
 		.extra2		= SYSCTL_ONE,
 	},
 #endif
+#ifdef CONFIG_SCHED_WALT
+	{
+		.procname	= "sched_lib_name",
+		.data		= sched_lib_name,
+		.maxlen		= LIB_PATH_LENGTH,
+		.mode		= 0644,
+		.proc_handler	= proc_dostring,
+	},
+	{
+		.procname	= "sched_lib_mask_force",
+		.data		= &sched_lib_mask_force,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_douintvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= &two_hundred_fifty_five,
+	},
+#endif
 #ifdef CONFIG_PROVE_LOCKING
 	{
 		.procname	= "prove_locking",
