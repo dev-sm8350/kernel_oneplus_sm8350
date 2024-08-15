874ccacf7377eaf3b27e28fe45c272a04659c809
TogoFire <togofire@mailfence.com>
2024-08-15 18:30:32 -0300

    Revert "treewide: Remove ipc logging support"
    This reverts commit bd1d4824a64e76be2f89190f895fbd07b3c46e6c.
    
    Reason: This breaks usb tethering.
    
    Log:
    ipa_usb ipa_usb_init_teth_prot_internal:814 Failed to initialize rndis_ipa
    
    Change-Id: I8c95bd271fd7018b4243cbb36829b40fb5154541
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/usb/gadget/function/f_gsi.c
+++ drivers/usb/gadget/function/f_gsi.c
@@ -3829,10 +3829,8 @@ static int gsi_set_inst_name(struct usb_function_instance *fi,
 {
 	int prot_id, name_len;
 	struct f_gsi *gsi;
-#ifdef CONFIG_IPC_LOGGING
 	char gsi_inst_name[MAX_INST_NAME_LEN + sizeof("gsi.") + 1];
 	void *ipc_log_ctxt;
-#endif
 	struct gsi_opts *opts, *opts_prev;
 	struct usb_os_desc *descs[1];
 	char *names[1];
@@ -3884,7 +3882,6 @@ static int gsi_set_inst_name(struct usb_function_instance *fi,
 		return PTR_ERR(gsi);
 
 	opts->gsi = gsi;
-#ifdef CONFIG_IPC_LOGGING
 	/*
 	 * create instance name with prefixing "gsi." to differentiate
 	 * ipc log debugfs entry
@@ -3895,7 +3892,6 @@ static int gsi_set_inst_name(struct usb_function_instance *fi,
 		pr_debug("%s: Err allocating ipc_log_ctxt for prot:%s\n",
 						__func__, gsi_inst_name);
 	opts->gsi->ipc_log_ctxt = ipc_log_ctxt;
-#endif
 
 	/* Set instance status */
 	mutex_lock(&inst_status[prot_id].gsi_lock);
@@ -3928,9 +3924,7 @@ static void gsi_free_inst(struct usb_function_instance *f)
 		return;
 	}
 
-#ifdef CONFIG_IPC_LOGGING
 	ipc_log_context_destroy(opts->gsi->ipc_log_ctxt);
-#endif
 	if (opts && opts->interf_group)
 		kfree(opts->interf_group);
 	/* Clear instance status */
