ab5684e881f574bb81d77455a90453612b1e48c0
TogoFire <togofire@mailfence.com>
2024-09-24 16:28:54 -0300

    sock: Security fix: Implement conditional PROT_SOCK
    * Implement conditional PROT_SOCK to prevent unauthorized access to privileged ports
    * By setting PROT_SOCK to 0, you are opening up the possibility for any process on the system to bind to these ports, potentially exposing the system to security risks. So only use this for Nethunter.
    
    Fix: "sock: Remove restrictions on port"
    Change-Id: 5df67da6c3d1dc492a071fcceb3ff86d2f5c809c
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1392,8 +1392,13 @@ static inline int __sk_prot_rehash(struct sock *sk)
 /* About 10 seconds */
 #define SOCK_DESTROY_TIME (10*HZ)
 
+#ifndef CONFIG_NETHUNTER_SUPPORT
 /* All sockets for all */
 #define PROT_SOCK	0
+#else
+/* Sockets 0-1023 can't be bound to unless you are superuser */
+#define PROT_SOCK	1024
+#endif
 
 #define SHUTDOWN_MASK	3
 #define RCV_SHUTDOWN	1
