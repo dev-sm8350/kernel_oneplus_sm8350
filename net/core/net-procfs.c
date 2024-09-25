56d93bae8b6bdc17eb115fcf62500c4d094d11f1
TogoFire <togofire@mailfence.com>
2024-09-25 16:37:07 -0300

    Revert "debloat: procfs"
    This reverts commit fd6932952022f968f2a1b49b48114df9d22e21c5.
    
    Change-Id: I1b8189024a9c1b54d8467c6900df13adf0e33e72
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- net/core/net-procfs.c
+++ net/core/net-procfs.c
@@ -308,12 +308,10 @@ static int __net_init dev_proc_net_init(struct net *net)
 	if (!proc_create_net("dev", 0444, net->proc_net, &dev_seq_ops,
 			sizeof(struct seq_net_private)))
 		goto out;
-	if (!IS_ENABLED(CONFIG_PROC_STRIPPED) &&
-			!proc_create_seq("softnet_stat", 0444, net->proc_net,
+	if (!proc_create_seq("softnet_stat", 0444, net->proc_net,
 			 &softnet_seq_ops))
 		goto out_dev;
-	if (!IS_ENABLED(CONFIG_PROC_STRIPPED) &&
-			!proc_create_net("ptype", 0444, net->proc_net, &ptype_seq_ops,
+	if (!proc_create_net("ptype", 0444, net->proc_net, &ptype_seq_ops,
 			sizeof(struct seq_net_private)))
 		goto out_softnet;
 
@@ -323,11 +321,9 @@ static int __net_init dev_proc_net_init(struct net *net)
 out:
 	return rc;
 out_ptype:
-	if (!IS_ENABLED(CONFIG_PROC_STRIPPED))
-		remove_proc_entry("ptype", net->proc_net);
+	remove_proc_entry("ptype", net->proc_net);
 out_softnet:
-	if (!IS_ENABLED(CONFIG_PROC_STRIPPED))
-		remove_proc_entry("softnet_stat", net->proc_net);
+	remove_proc_entry("softnet_stat", net->proc_net);
 out_dev:
 	remove_proc_entry("dev", net->proc_net);
 	goto out;
@@ -337,10 +333,8 @@ static void __net_exit dev_proc_net_exit(struct net *net)
 {
 	wext_proc_exit(net);
 
-	if (!IS_ENABLED(CONFIG_PROC_STRIPPED)) {
-		remove_proc_entry("ptype", net->proc_net);
-		remove_proc_entry("softnet_stat", net->proc_net);
-	}
+	remove_proc_entry("ptype", net->proc_net);
+	remove_proc_entry("softnet_stat", net->proc_net);
 	remove_proc_entry("dev", net->proc_net);
 }
 
