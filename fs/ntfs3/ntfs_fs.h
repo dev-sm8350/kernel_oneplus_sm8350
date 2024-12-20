788b849f6b13e21542dff4a88c16a1d4509123a6
psndna88 <psndna88@gmail.com>
2024-12-20 13:47:57 +0530

    fs: ntfs3: upgrade from 5.15.175
    fs: ntfs3: Remove bitmap_size function
    fs: ntfs3: apply backport compatibility 5.4.y
    fs: ntfs3: register filesystem as "ntfs" & "ntfs3" both
    fs: ntfs3: apply backport compatibility 5.10.y
    fs: ntfs3: import from 5.15.175
    
    Change-Id: 474cf16554ef48e38b64867b472928da75821121
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- fs/ntfs3/ntfs_fs.h
+++ fs/ntfs3/ntfs_fs.h
@@ -198,6 +198,8 @@ struct ntfs_index {
 
 /* Minimum MFT zone. */
 #define NTFS_MIN_MFT_ZONE 100
+/* Step to increase the MFT. */
+#define NTFS_MFT_INCREASE_STEP 1024
 
 /* Ntfs file system in-core superblock data. */
 struct ntfs_sb_info {
@@ -326,7 +328,7 @@ struct mft_inode {
 
 /* Nested class for ntfs_inode::ni_lock. */
 enum ntfs_inode_mutex_lock_class {
-	NTFS_INODE_MUTEX_DIRTY,
+	NTFS_INODE_MUTEX_DIRTY = 1,
 	NTFS_INODE_MUTEX_SECURITY,
 	NTFS_INODE_MUTEX_OBJID,
 	NTFS_INODE_MUTEX_REPARSE,
