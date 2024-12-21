5943cab67e344dac5717a94eb02056fd636cb6cd
TogoFire <togofire@mailfence.com>
2024-12-21 16:49:00 -0300

    post_boot: align code and cleanup
    Change-Id: f09ec7baf2551f25fe5f177b343b08d430d36b44
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- scripts/init.kernel.post_boot-lahaina.sh
+++ scripts/init.kernel.post_boot-lahaina.sh
@@ -1,7 +1,7 @@
 #! /vendor/bin/sh
 
 #=============================================================================
-# Copyright (c) 2020 Qualcomm Technologies, Inc.
+# Copyright (c) 2020-2021 Qualcomm Technologies, Inc.
 # All Rights Reserved.
 # Confidential and Proprietary - Qualcomm Technologies, Inc.
 #
@@ -37,8 +37,8 @@ exec > /dev/kmsg 2>&1
 echo "execprog: sh execution"
 
 if getprop | grep -q 'ro\.voltage\.'; then
-  echo "execprog: VoltageOS detected, skipping execution"
-  exit 0
+    echo "execprog: VoltageOS detected, skipping execution"
+    exit 0
 fi
 
 BIND=/vendor/bin/init.kernel.post_boot-lahaina.sh
@@ -48,86 +48,83 @@ ddr_type4="07"
 ddr_type5="08"
 
 if ! mount | grep -q "$BIND" && [ ! -e /sbin/recovery ] && [ ! -e /dev/ep/.post_boot ]; then
-  echo "execprog: restarting under tmpfs"
-  # Run under a new tmpfs to avoid /dev selabel
-  mkdir /dev/ep
-  mount -t tmpfs nodev /dev/ep
-  touch /dev/ep/.post_boot
-  cp -p /dev/execprog /dev/ep/execprog
-  rm /dev/execprog
-  chown root:shell /dev/ep/execprog
+    echo "execprog: restarting under tmpfs"
+    # Run under a new tmpfs to avoid /dev selabel
+    mkdir /dev/ep
+    mount -t tmpfs nodev /dev/ep
+    touch /dev/ep/.post_boot
+    cp -p /dev/execprog /dev/ep/execprog
+    rm /dev/execprog
+    chown root:shell /dev/ep/execprog
 
-  mount --bind /dev/ep/execprog "$BIND"
-  chcon "u:object_r:vendor_file:s0" "$BIND"
+    mount --bind /dev/ep/execprog "$BIND"
+    chcon "u:object_r:vendor_file:s0" "$BIND"
 
-  # Optimize LMKD Settings for Background App Management
-  # Temporary directory
-  TEMP_DIR=/dev/ep
-  # Copy the original file
-  cp -p /vendor/etc/perf/perfconfigstore.xml $TEMP_DIR/perfconfigstore.xml
-  cp -p /vendor/etc/perf/perfconfigstore.xml "$TEMP_DIR/perfconfigstore.xml"
-  # Function to add or replace a line
-  add_or_replace() {
+    # Optimize LMKD Settings for Background App Management
+    # Temporary directory
+    TEMP_DIR=/dev/ep
+    # Copy the original file
+    cp -p /vendor/etc/perf/perfconfigstore.xml $TEMP_DIR/perfconfigstore.xml
+    cp -p /vendor/etc/perf/perfconfigstore.xml "$TEMP_DIR/perfconfigstore.xml"
+    # Function to add or replace a line
+    add_or_replace() {
     local prop_name="$1"
     local prop_value="$2"
     sed -i "/<Prop Name=\"$prop_name\"/d" $TEMP_DIR/perfconfigstore.xml
     echo "<Prop Name=\"$prop_name\" Value=\"$prop_value\" />" >> $TEMP_DIR/perfconfigstore.xml
-  }
-  # Replace or add desired lines
-  add_or_replace "ro.vendor.qti.sys.fw.bg_apps_limit" "224"
-  add_or_replace "ro.lmk.kill_timeout_ms_dup" "350"
-  add_or_replace "ro.lmk.thrashing_threshold" "32"
-  add_or_replace "ro.lmk.psi_scrit_complete_stall_ms" "400"
-  add_or_replace "ro.lmk.super_critical" "850"
-  add_or_replace "vendor.appcompact.enable_app_compact" "false"
-  add_or_replace "ro.lmk.use_new_strategy_dup" "true"
-  add_or_replace "ro.lmk.nandswap_version" "2"
+    }
+    # Replace or add desired lines
+    add_or_replace "ro.vendor.qti.sys.fw.bg_apps_limit" "224"
+    add_or_replace "ro.lmk.kill_timeout_ms_dup" "350"
+    add_or_replace "ro.lmk.thrashing_threshold" "32"
+    add_or_replace "ro.lmk.psi_scrit_complete_stall_ms" "400"
+    add_or_replace "ro.lmk.super_critical" "850"
+    add_or_replace "vendor.appcompact.enable_app_compact" "false"
+    add_or_replace "ro.lmk.use_new_strategy_dup" "true"
+    add_or_replace "ro.lmk.nandswap_version" "2"
 
-  # Remove the useless lines
-  sed -i '/<Prop Name="ro.vendor.perf.qhf" Value="true" Target="lahaina">/d' $TEMP_DIR/perfconfigstore.xml
-  sed -i '/<Prop Name="ro.vendor.perf.qape" Value="true" Target="lahaina">/d' $TEMP_DIR/perfconfigstore.xml
-  sed -i '/<Prop Name="ro.vendor.perf.qgpe" Value="true">/d' $TEMP_DIR/perfconfigstore.xml
+    # Remove the useless lines
+    sed -i '/<Prop Name="ro.vendor.perf.qhf" Value="true" Target="lahaina">/d' $TEMP_DIR/perfconfigstore.xml
+    sed -i '/<Prop Name="ro.vendor.perf.qape" Value="true" Target="lahaina">/d' $TEMP_DIR/perfconfigstore.xml
+    sed -i '/<Prop Name="ro.vendor.perf.qgpe" Value="true">/d' $TEMP_DIR/perfconfigstore.xml
 
-  # Replace the original file
-  cp "$TEMP_DIR/perfconfigstore.xml" /vendor/etc/perf/perfconfigstore.xml
-  # Correct security context
-  chcon "u:object_r:vendor_configs_file:s0" /vendor/etc/perf/perfconfigstore.xml
-  ## END Optimize LMKD ##
+    # Replace the original file
+    cp "$TEMP_DIR/perfconfigstore.xml" /vendor/etc/perf/perfconfigstore.xml
+    # Correct security context
+    chcon "u:object_r:vendor_configs_file:s0" /vendor/etc/perf/perfconfigstore.xml
+    ## END Optimize LMKD ##
 
-  # lazy unmount /dev/ep for invisibility
-  umount -l /dev/ep
+    # lazy unmount /dev/ep for invisibility
+    umount -l /dev/ep
 
-  # Setup swap
-  while [ ! -e /dev/block/zram0 ]; do
-    sleep 1
-  done
-  if ! grep -q zram /proc/swaps; then
-    MemStr=$(cat /proc/meminfo | grep MemTotal)
-    MemKb=$((${MemStr:16:8}))
+    # Setup swap
+    while [ ! -e /dev/block/zram0 ]; do
+        sleep 1
+    done
+    if ! grep -q zram /proc/swaps; then
+        MemStr=$(cat /proc/meminfo | grep MemTotal)
+        MemKb=$((${MemStr:16:8}))
 
-    # Set swap size to half of MemTotal
-    # Align by 4 MiB
-    expr $MemKb / 2 '*' 1024 / 4194304 '*' 4194304 > /sys/block/zram0/disksize
+        # Set swap size to half of MemTotal
+        # Align by 4 MiB
+        expr $MemKb / 2 '*' 1024 / 4194304 '*' 4194304 > /sys/block/zram0/disksize
 
-    mkswap /dev/block/zram0
-    swapon /dev/block/zram0
-  fi
+        mkswap /dev/block/zram0
+        swapon /dev/block/zram0
+    fi
 
-  # Disable read-ahead swap
-  echo 0 > /proc/sys/vm/page-cluster
+    # Disable read-ahead swap
+    echo 0 > /proc/sys/vm/page-cluster
 
-  # configure input boost settings
-  chown system:system /sys/devices/system/cpu/cpu_boost/input_boost_freq
-  echo "0:1401600" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
-  echo 200 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
+    # configure input boost settings
+    chown system:system /sys/devices/system/cpu/cpu_boost/input_boost_freq
+    echo "0:1401600" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
+    echo 200 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
 
-  #liochen@SYSTEM, 2020/11/02, Add for enable ufs performance
-  echo 0 > /sys/class/scsi_host/host0/../../../clkscale_enable
+    #liochen@SYSTEM, 2020/11/02, Add for enable ufs performance
+    echo 0 > /sys/class/scsi_host/host0/../../../clkscale_enable
 
-  # SSG
-  echo 25 > /dev/blkio/background/blkio.ssg.max_available_ratio
-
-  exit
+    exit
 fi
 
 # Setting b.L scheduler parameters
@@ -162,114 +159,111 @@ echo 0 > /sys/devices/system/cpu/cpufreq/policy7/schedutil/rate_limit_us
 
 # configure bus-dcvs
 device=/sys/devices/platform/soc
-for cpubw in $device/*cpu-cpu-llcc-bw/devfreq/*cpu-cpu-llcc-bw
-do
-    cat $cpubw/available_frequencies | cut -d " " -f 1 > $cpubw/min_freq
-    echo "4577 7110 9155 12298 14236 15258" > $cpubw/bw_hwmon/mbps_zones
-    echo 4 > $cpubw/bw_hwmon/sample_ms
-    echo 80 > $cpubw/bw_hwmon/io_percent
-    echo 20 > $cpubw/bw_hwmon/hist_memory
-    echo 10 > $cpubw/bw_hwmon/hyst_length
-    echo 30 > $cpubw/bw_hwmon/down_thres
-    echo 0 > $cpubw/bw_hwmon/guard_band_mbps
-    echo 250 > $cpubw/bw_hwmon/up_scale
-    echo 1600 > $cpubw/bw_hwmon/idle_mbps
-    echo 12298 > $cpubw/max_freq
-    echo 40 > $cpubw/polling_interval
-done
+	for cpubw in $device/*cpu-cpu-llcc-bw/devfreq/*cpu-cpu-llcc-bw
+	do
+		cat $cpubw/available_frequencies | cut -d " " -f 1 > $cpubw/min_freq
+		echo "4577 7110 9155 12298 14236 15258" > $cpubw/bw_hwmon/mbps_zones
+		echo 4 > $cpubw/bw_hwmon/sample_ms
+		echo 80 > $cpubw/bw_hwmon/io_percent
+		echo 20 > $cpubw/bw_hwmon/hist_memory
+		echo 10 > $cpubw/bw_hwmon/hyst_length
+		echo 30 > $cpubw/bw_hwmon/down_thres
+		echo 0 > $cpubw/bw_hwmon/guard_band_mbps
+		echo 250 > $cpubw/bw_hwmon/up_scale
+		echo 1600 > $cpubw/bw_hwmon/idle_mbps
+		echo 12298 > $cpubw/max_freq
+		echo 40 > $cpubw/polling_interval
+	done
 
-for llccbw in $device/*cpu-llcc-ddr-bw/devfreq/*cpu-llcc-ddr-bw
-do
-    cat $llccbw/available_frequencies | cut -d " " -f 1 > $llccbw/min_freq
-    if [ ${ddr_type:4:2} == $ddr_type4 ]; then
-        echo "1720 2086 2929 3879 5931 6515 8136" > $llccbw/bw_hwmon/mbps_zones
-    elif [ ${ddr_type:4:2} == $ddr_type5 ]; then
-        echo "1720 2086 2929 3879 6515 7980 12191" > $llccbw/bw_hwmon/mbps_zones
-    fi
-    echo 4 > $llccbw/bw_hwmon/sample_ms
-    echo 80 > $llccbw/bw_hwmon/io_percent
-    echo 20 > $llccbw/bw_hwmon/hist_memory
-    echo 10 > $llccbw/bw_hwmon/hyst_length
-    echo 30 > $llccbw/bw_hwmon/down_thres
-    echo 0 > $llccbw/bw_hwmon/guard_band_mbps
-    echo 250 > $llccbw/bw_hwmon/up_scale
-    echo 1600 > $llccbw/bw_hwmon/idle_mbps
-    echo 6515 > $llccbw/max_freq
-    echo 40 > $llccbw/polling_interval
-done
+	for llccbw in $device/*cpu-llcc-ddr-bw/devfreq/*cpu-llcc-ddr-bw
+	do
+		cat $llccbw/available_frequencies | cut -d " " -f 1 > $llccbw/min_freq
+		if [ ${ddr_type:4:2} == $ddr_type4 ]; then
+		echo "1720 2086 2929 3879 5931 6515 8136" > $llccbw/bw_hwmon/mbps_zones
+		elif [ ${ddr_type:4:2} == $ddr_type5 ]; then
+		echo "1720 2086 2929 3879 6515 7980 12191" > $llccbw/bw_hwmon/mbps_zones
+		fi
+		echo 4 > $llccbw/bw_hwmon/sample_ms
+		echo 80 > $llccbw/bw_hwmon/io_percent
+		echo 20 > $llccbw/bw_hwmon/hist_memory
+		echo 10 > $llccbw/bw_hwmon/hyst_length
+		echo 30 > $llccbw/bw_hwmon/down_thres
+		echo 0 > $llccbw/bw_hwmon/guard_band_mbps
+		echo 250 > $llccbw/bw_hwmon/up_scale
+		echo 1600 > $llccbw/bw_hwmon/idle_mbps
+		echo 6515 > $llccbw/max_freq
+		echo 40 > $llccbw/polling_interval
+	done
 
-for l3bw in $device/*snoop-l3-bw/devfreq/*snoop-l3-bw
-do
-    cat $l3bw/available_frequencies | cut -d " " -f 1 > $l3bw/min_freq
-    echo 4 > $l3bw/bw_hwmon/sample_ms
-    echo 10 > $l3bw/bw_hwmon/io_percent
-    echo 20 > $l3bw/bw_hwmon/hist_memory
-    echo 10 > $l3bw/bw_hwmon/hyst_length
-    echo 0 > $l3bw/bw_hwmon/down_thres
-    echo 0 > $l3bw/bw_hwmon/guard_band_mbps
-    echo 0 > $l3bw/bw_hwmon/up_scale
-    echo 1600 > $l3bw/bw_hwmon/idle_mbps
-    echo 9155 > $l3bw/max_freq
-    echo 40 > $l3bw/polling_interval
-done
+	for l3bw in $device/*snoop-l3-bw/devfreq/*snoop-l3-bw
+	do
+		cat $l3bw/available_frequencies | cut -d " " -f 1 > $l3bw/min_freq
+		echo 4 > $l3bw/bw_hwmon/sample_ms
+		echo 10 > $l3bw/bw_hwmon/io_percent
+		echo 20 > $l3bw/bw_hwmon/hist_memory
+		echo 10 > $l3bw/bw_hwmon/hyst_length
+		echo 0 > $l3bw/bw_hwmon/down_thres
+		echo 0 > $l3bw/bw_hwmon/guard_band_mbps
+		echo 0 > $l3bw/bw_hwmon/up_scale
+		echo 1600 > $l3bw/bw_hwmon/idle_mbps
+		echo 9155 > $l3bw/max_freq
+		echo 40 > $l3bw/polling_interval
+	done
 
-# configure mem_latency settings for LLCC and DDR scaling and qoslat
-for memlat in $device/*lat/devfreq/*lat
-do
-    cat $memlat/available_frequencies | cut -d " " -f 1 > $memlat/min_freq
-    echo 8 > $memlat/polling_interval
-    echo 400 > $memlat/mem_latency/ratio_ceil
-done
+	# configure mem_latency settings for LLCC and DDR scaling and qoslat
+	for memlat in $device/*lat/devfreq/*lat
+	do
+		cat $memlat/available_frequencies | cut -d " " -f 1 > $memlat/min_freq
+		echo 8 > $memlat/polling_interval
+		echo 400 > $memlat/mem_latency/ratio_ceil
+	done
 
-# configure compute settings for gold latfloor
-for latfloor in $device/*cpu4-cpu*latfloor/devfreq/*cpu4-cpu*latfloor
-do
-    cat $latfloor/available_frequencies | cut -d " " -f 1 > $latfloor/min_freq
-    echo 8 > $latfloor/polling_interval
-done
+	# configure compute settings for gold latfloor
+	for latfloor in $device/*cpu4-cpu*latfloor/devfreq/*cpu4-cpu*latfloor
+	do
+		cat $latfloor/available_frequencies | cut -d " " -f 1 > $latfloor/min_freq
+		echo 8 > $latfloor/polling_interval
+	done
 
-# configure mem_latency settings for prime latfloor
-for latfloor in $device/*cpu7-cpu*latfloor/devfreq/*cpu7-cpu*latfloor
-do
-    cat $latfloor/available_frequencies | cut -d " " -f 1 > $latfloor/min_freq
-    echo 8 > $latfloor/polling_interval
-    echo 25000 > $latfloor/mem_latency/ratio_ceil
-done
+	# configure mem_latency settings for prime latfloor
+	for latfloor in $device/*cpu7-cpu*latfloor/devfreq/*cpu7-cpu*latfloor
+	do
+		cat $latfloor/available_frequencies | cut -d " " -f 1 > $latfloor/min_freq
+		echo 8 > $latfloor/polling_interval
+		echo 25000 > $latfloor/mem_latency/ratio_ceil
+	done
 
-# CPU4 L3 ratio ceil
-for l3gold in $device/*cpu4-cpu-l3-lat/devfreq/*cpu4-cpu-l3-lat
-do
-    echo 4000 > $l3gold/mem_latency/ratio_ceil
-done
+	# CPU4 L3 ratio ceil
+	for l3gold in $device/*cpu4-cpu-l3-lat/devfreq/*cpu4-cpu-l3-lat
+	do
+		echo 4000 > $l3gold/mem_latency/ratio_ceil
+	done
 
-# CPU5 L3 ratio ceil
-for l3gold in $device/*cpu5-cpu-l3-lat/devfreq/*cpu5-cpu-l3-lat
-do
-    echo 4000 > $l3gold/mem_latency/ratio_ceil
-done
+	# CPU5 L3 ratio ceil
+	for l3gold in $device/*cpu5-cpu-l3-lat/devfreq/*cpu5-cpu-l3-lat
+	do
+		echo 4000 > $l3gold/mem_latency/ratio_ceil
+	done
 
-# CPU6 L3 ratio ceil
-for l3gold in $device/*cpu6-cpu-l3-lat/devfreq/*cpu6-cpu-l3-lat
-do
-    echo 4000 > $l3gold/mem_latency/ratio_ceil
-done
+	# CPU6 L3 ratio ceil
+	for l3gold in $device/*cpu6-cpu-l3-lat/devfreq/*cpu6-cpu-l3-lat
+	do
+		echo 4000 > $l3gold/mem_latency/ratio_ceil
+	done
 
-# prime L3 ratio ceil
-for l3prime in $device/*cpu7-cpu-l3-lat/devfreq/*cpu7-cpu-l3-lat
-do
-    echo 20000 > $l3prime/mem_latency/ratio_ceil
-done
+	# prime L3 ratio ceil
+	for l3prime in $device/*cpu7-cpu-l3-lat/devfreq/*cpu7-cpu-l3-lat
+	do
+		echo 20000 > $l3prime/mem_latency/ratio_ceil
+	done
 
-# qoslat ratio ceil
-for qoslat in $device/*qoslat/devfreq/*qoslat
-do
-    echo 50 > $qoslat/mem_latency/ratio_ceil
-done
+	# qoslat ratio ceil
+	for qoslat in $device/*qoslat/devfreq/*qoslat
+	do
+		echo 50 > $qoslat/mem_latency/ratio_ceil
+	done
 
 echo N > /sys/module/lpm_levels/parameters/sleep_disabled
-
-# Setup readahead
-find /sys/devices -name read_ahead_kb | while read node; do echo 128 > $node; done
 
 # Remove unused swapfile
 rm -rf /data/vendor/swap
@@ -277,18 +271,18 @@ sync
 
 # Let kernel know our image version/variant/crm_version
 if [ -f /sys/devices/soc0/select_image ]; then
-    image_version="10:"
-    image_version+=`getprop ro.build.id`
-    image_version+=":"
-    image_version+=`getprop ro.build.version.incremental`
-    image_variant=`getprop ro.product.name`
-    image_variant+="-"
-    image_variant+=`getprop ro.build.type`
-    oem_version=`getprop ro.build.version.codename`
-    echo 10 > /sys/devices/soc0/select_image
-    echo $image_version > /sys/devices/soc0/image_version
-    echo $image_variant > /sys/devices/soc0/image_variant
-    echo $oem_version > /sys/devices/soc0/image_crm_version
+	image_version="10:"
+	image_version+=`getprop ro.build.id`
+	image_version+=":"
+	image_version+=`getprop ro.build.version.incremental`
+	image_variant=`getprop ro.product.name`
+	image_variant+="-"
+	image_variant+=`getprop ro.build.type`
+	oem_version=`getprop ro.build.version.codename`
+	echo 10 > /sys/devices/soc0/select_image
+	echo $image_version > /sys/devices/soc0/image_version
+	echo $image_variant > /sys/devices/soc0/image_variant
+	echo $oem_version > /sys/devices/soc0/image_crm_version
 fi
 
 setprop vendor.post_boot.parsed 1
