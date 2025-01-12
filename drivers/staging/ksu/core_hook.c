6ce25de9d13610389abaf1ae69061af664ffa67d
TogoFire <togofire@mailfence.com>
2025-01-12 14:08:07 -0300

    drivers: staging: ksu: Remove umount vendor to lahaina
    ref: https://github.com/LineageOS/android_device_oneplus_sm8350-common/commit/87b0b0aaedc104b67226ba300162a971be5bf7cc
    
    Does not support due to erofs.
    
    Log:
    KernelSU: umount /vendor failed: -16
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ia74e8432f397ce26bd2a11546ea10966cb06f9c8

--- drivers/staging/ksu/core_hook.c
+++ drivers/staging/ksu/core_hook.c
@@ -1105,7 +1105,6 @@ out_ksu_try_umount:
 	// fixme: use `collect_mounts` and `iterate_mount` to iterate all mountpoint and
 	// filter the mountpoint whose target is `/data/adb`
 	ksu_try_umount("/system", true, 0);
-	ksu_try_umount("/vendor", true, 0);
 	ksu_try_umount("/product", true, 0);
 	ksu_try_umount("/system_ext", true, 0);
 	ksu_try_umount("/data/adb/modules", false, MNT_DETACH);
