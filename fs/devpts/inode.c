fd4815919e9044ca80467aa300446431b169efeb
simonpunk <simonpunk2016@gmail.com>
2025-02-04 11:01:49 +0800

    fs/include/kernel: Import susfs4ksu 1.5.5 to k5.4
    ref: https://gitlab.com/simonpunk/susfs4ksu/-/commits/6825f558d0213f933d375e3d6be98d4d0a73d706
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I04fbf05b34382074880c810fdfeb048f61d28322

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -600,6 +600,10 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 extern int ksu_handle_devpts(struct inode*);
 #endif
 
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+extern bool ksu_devpts_hook;
+#endif
+
 /**
  * devpts_get_priv -- get private data for a slave
  * @pts_inode: inode of the slave
@@ -611,6 +615,13 @@ void *devpts_get_priv(struct dentry *dentry)
 #ifdef CONFIG_KSU
 	ksu_handle_devpts(dentry->d_inode);
 #endif
+
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+	if (likely(ksu_devpts_hook)) {
+		ksu_handle_devpts(dentry->d_inode);
+	}
+#endif
+
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
