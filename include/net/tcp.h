d21a18f51de58c2ff31ea01d621541321c0c2c17
Madara273 <ravenhoxs@gmail.com>
2025-02-22 19:59:06 +0000

    MPTCP -- tcp: move RST on ABORT according to RFC793 to the header file
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Change-Id: I605c370da2497a113ea446a906360b513f1242aa
    (cherry picked from commit 96e11e56e01b04e59781a70af4e8a2c3629e0a99)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -428,7 +428,7 @@ static inline bool tcp_need_reset(int state)
 {
 	return (1 << state) &
 	       (TCPF_ESTABLISHED | TCPF_CLOSE_WAIT | TCPF_FIN_WAIT1 |
-		TCPF_FIN_WAIT2 | TCPF_SYN_RECV);
+		TCPF_FIN_WAIT2 | TCPF_SYN_RECV | TCPF_SYN_SENT);
 }
 
 int __must_check tcp_queue_rcv(struct sock *sk, struct sk_buff *skb,
