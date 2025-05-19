d9261e7d3074ad1a3f1139db8081b3a595539a57
Brandon Kammerdiener <brandon.kammerdiener@intel.com>
2025-05-19 11:18:59 +0000

    bpf: fix possible endless loop in BPF map iteration
    The _safe variant used here gets the next element before running the callback,
    avoiding the endless loop condition.
    
    Signed-off-by: Brandon Kammerdiener <brandon.kammerdiener@intel.com>
    Change-Id: I14b3b7f23a2f042b56c75758cc65edad790db89f
    Link: https://lore.kernel.org/r/20250424153246.141677-2-brandon.kammerdiener@intel.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Hou Tao <houtao1@huawei.com>
    Signed-off-by: Kanishk <kanishkthederp@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- kernel/bpf/hashtab.c
+++ kernel/bpf/hashtab.c
@@ -431,7 +431,7 @@ static struct htab_elem *lookup_elem_raw(struct hlist_nulls_head *head, u32 hash
 	struct hlist_nulls_node *n;
 	struct htab_elem *l;
 
-	hlist_nulls_for_each_entry_rcu(l, n, head, hash_node)
+	hlist_nulls_for_each_entry_safe(l, n, head, hash_node)
 		if (l->hash == hash && !memcmp(&l->key, key, key_size))
 			return l;
 
